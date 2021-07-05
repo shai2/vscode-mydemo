f7c2afc19fb18451dd6ddbb9a730027a92ac4936
shai2 <464740437@qq.com>
2021-07-05 15:30:57 +0800

    fix报错

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+const vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+exports.activate = (context) => {
   console.log('“vscode-mydemo”激活');
   // 自动补全代码
   require('./provide-completion')(context);
-}
+};
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+exports.deactivate = () => {};
