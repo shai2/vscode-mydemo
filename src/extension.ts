a8361be530b12812fc68bdeb1e457e02c8b4766d
shai2 <464740437@qq.com>
2021-07-05 14:32:21 +0800

    存档

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('“vscode-mydemo”激活');
   // 自动补全代码
   require('./provide-completion')(context);
 }
