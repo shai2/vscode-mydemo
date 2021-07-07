ad63d5a5a3819e13553788b525e4b87faba36626
shai2 <464740437@qq.com>
2021-07-07 21:25:54 +0800

    存档

--- extension.js
+++ extension.js
@@ -13,20 +13,18 @@ function activate(context) {
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vscode-myplugin" is now active!'
-  );
+  console.log('Congratulations, your extension "shai2" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'vscode-myplugin.helloWorld',
+    'shai2.helloWorld',
     function () {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from vscode-myplugin!');
+      vscode.window.showInformationMessage('Hello World from shai2!');
     }
   );
 
